248ad6723af1bdfd490dfa23f816171aa8687ebf
VTNhan <vtn12032004@gmail.com>
2025-02-26 19:22:21 +0700

    Lan 1

---
+++ Bai11/Bai11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Bai11
+{
+    internal class Program
+    {
+
+        static void BookInfo(string title, string author, int year = -1, double price = 0)
+        {
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Author: {author}");
+            Console.WriteLine($"Year: {(year == -1 ? "Unknown" : year.ToString())}");
+            Console.WriteLine($"Price: {(price == 0 ? "Free" : "$" + price)}");
+            Console.WriteLine("----------------------");
+        }
+        private static void Main(string[] args)
+        {
+            BookInfo("Harry Porter", "J.K Rowling", 1980, 10.99);
+            BookInfo("To Kill a Mockingbird", "Harper Lee", price: 12.5);
+            BookInfo(" Peter Pan", "J. M. Barrie");
+        }
+    }
+}
