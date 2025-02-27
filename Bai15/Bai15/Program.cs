29a3240fb5df11749feaa81f7e1b93a9d2fea39c
VTNhan <vtn12032004@gmail.com>
2025-02-27 13:46:54 +0700

    lan2

--- Bai15/Bai15/Program.cs
+++ Bai15/Bai15/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public enum PointColor
+{
+    LightBlue,
+    BloodRed,
+    Gold
+}
+
+internal class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public PointColor Color { get; set; }
+
+    public Point(int x, int y, PointColor color)
+    {
+        X = x;
+        Y = y;
+        Color = color;
+    }
+ 
+
+    public void DisplayStatus()
+    {
+        Console.WriteLine($"Point({X}, {Y}) - Color: {Color}");
+    }
+    private static void Main(string[] args)
+    {
+        Point p1 = new Point(1,5,PointColor.LightBlue);
+        Point p2 = new Point(4, 2, PointColor.BloodRed);
+
+        p1.DisplayStatus();
+        p2.DisplayStatus();
+    }
+}
--- Bai15/Bai15/obj/Debug/net8.0/Bai15.AssemblyInfo.cs
+++ Bai15/Bai15/obj/Debug/net8.0/Bai15.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64244e89acf0c876a03471864a62847ea037e420")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
