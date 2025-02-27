ce962bd8efafc0f505e31775707a8d4ecc75f0c0
VTNhan <vtn12032004@gmail.com>
2025-02-27 14:19:14 +0700

    Lan 3

---
+++ Bai15/Bai15/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai15
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointColor Color { get; set; }
+
+        public Point(int x, int y, PointColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Point({X}, {Y}) - Color: {Color}");
+        }
+    }
+   
+}
---
+++ Bai15/Bai15/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai15
+{
+    public class Rectangle
+    {
+        public Point UpperLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public Rectangle(Point upperLeft, Point bottomRight)
+        {
+            UpperLeft = upperLeft;
+            BottomRight = bottomRight;
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Rectangle Status:");
+            Console.Write("Upper Left: ");
+            UpperLeft.DisplayStatus();
+            Console.Write("Bottom Right: ");
+            BottomRight.DisplayStatus();
+        }
+    }
+}
