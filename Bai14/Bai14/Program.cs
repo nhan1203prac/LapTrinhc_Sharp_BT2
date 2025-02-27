7c92938e1bafd6a487a064ff5ad706271a7ffc15
VTNhan <vtn12032004@gmail.com>
2025-02-27 13:28:02 +0700

    lan2

--- Bai14/Bai14/Program.cs
+++ Bai14/Bai14/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhập số lượng học sinh: ");
+        int soLuong = int.Parse(Console.ReadLine());
+
+        double[] diemHocSinh = new double[soLuong];
+        for (int i = 0; i < soLuong; i++)
+        {
+            Console.Write($"Nhập điểm của học sinh thứ {i + 1}: ");
+            diemHocSinh[i] = double.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nDanh sách điểm số:");
+        foreach (double diem in diemHocSinh)
+        {
+            Console.WriteLine(diem);
+        }
+        double tongDiem = 0;
+        foreach (double diem in diemHocSinh)
+        {
+            tongDiem += diem;
+        }
+        double diemTrungBinh = tongDiem / soLuong;
+
+        Console.WriteLine($"\nĐiểm trung bình của lớp: {diemTrungBinh:F2}");
+    }
+}
--- Bai14/Bai14/obj/Debug/net8.0/Bai14.AssemblyInfo.cs
+++ Bai14/Bai14/obj/Debug/net8.0/Bai14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb8ea0049b6c69320e9d2e00da92fb03910c03f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
