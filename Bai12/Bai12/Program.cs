7275da4fff87c63a438042a93463ee5e4b09f89e
VTNhan <vtn12032004@gmail.com>
2025-02-27 18:13:03 +0700

    Lan3

--- Bai12/Bai12/Program.cs
+++ Bai12/Bai12/Program.cs
@@ -12,19 +12,19 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("===== MENU =====");
-        foreach (MonAn mon in Enum.GetValues(typeof(MonAn)))
+        foreach (MonAn mon_234 in Enum.GetValues(typeof(MonAn)))
         {
-            Console.WriteLine($"{(int)mon}. {mon}");
+            Console.WriteLine($"{(int)mon_234}. {mon_234}");
         }
-        Console.Write("\nNhập số tương ứng với món ăn bạn muốn chọn: ");
-        if (int.TryParse(Console.ReadLine(), out int luaChon) && Enum.IsDefined(typeof(MonAn), luaChon))
+        Console.Write("\nNhap so tuong ung voi mon an ban muon chon: ");
+        if (int.TryParse(Console.ReadLine(), out int luaChon_234) && Enum.IsDefined(typeof(MonAn), luaChon_234))
         {
-            MonAn monDaChon = (MonAn)luaChon;
-            Console.WriteLine($"Bạn đã chọn món: {monDaChon}");
+            MonAn monDaChon = (MonAn)luaChon_234;
+            Console.WriteLine($"Ban đa chon mon: {monDaChon}");
         }
         else
         {
-            Console.WriteLine("Lựa chọn không hợp lệ!");
+            Console.WriteLine("Lua chon khong hop le!");
         }
     }
 }
